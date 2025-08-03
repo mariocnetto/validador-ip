b8196398bc674c4c4f198717c91e9ce9e069fa94
mariocnetto <mariocoutonetto@gmail.com>
2025-08-03 02:04:11 -0300

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const PORT = process.env.PORT || 3000;
 
 const IPS_AUTORIZADOS = [
   '177.43.148.75',  // Substitua pelo IP real
-  '189.52.22.10',
+  '162.120.185.212',
   '162.120.185.212',  // Apollo
 ];
 
