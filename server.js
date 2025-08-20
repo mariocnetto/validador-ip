c9e04e6946d6403b7230cea1a892bac40d15c5ef
mariocnetto <mariocoutonetto@gmail.com>
2025-08-20 02:10:21 -0300

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 const IPS_AUTORIZADOS = [
-  '104.28.193.83',  // meu ip descartavel
+  '96da4960d916f77c4c7b9546f39843c61702bf30e7a392e29de77f9665c60c2a',  // meu ip descartavel
   '177.158.146.156',  // Meu IP
   '187.109.129.210',  // camara
   '104.28.193.83',  // meu ip descartavel2
